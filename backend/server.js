89ef41e4ac515f5a1de5aca85d58ac0ac39e560e
Koppino <60857913+Koppino@users.noreply.github.com>
2025-04-09 14:28:53 +0200

    ...

--- backend/server.js
+++ backend/server.js
@@ -30,10 +30,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.use(express.static(path.join(__dirname, "/frontend/dist2")));
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "dist2", "index.html"));
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
 server.listen(PORT, () => {
